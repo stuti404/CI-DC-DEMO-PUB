f16fd61da181ecb59fcb76c41428ad9efca756f7
Stuti-Ganit <Stuti@ganitinc.com>
2026-08-24 16:01:19 +0530

    Clean up: PFL/notebooks/smoke_test.py

---
+++ PFL/notebooks/smoke_test.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+dbutils.widgets.text("environment", "dev")
+env = dbutils.widgets.get("environment")
+
+# COMMAND ----------
+
+print(f"Running smoke test in '{env}'")
+
+result = 1 + 1
+assert result == 2
+
+second_check = 2 * 3
+assert second_check == 6
+
+print("Smoke test passed")
