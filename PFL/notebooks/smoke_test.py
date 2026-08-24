4d69237b0ce8fbf95df729ff3b308a5727e9d986
Stuti-Ganit <Stuti@ganitinc.com>
2026-08-24 18:09:12 +0530

    feature/ci-cd-S1 to dev (#30)
    * Feature branch change
    
    * PR for code changes
    
    * Add Developer changes to DEV

--- PFL/notebooks/smoke_test.py
+++ PFL/notebooks/smoke_test.py
@@ -6,10 +6,11 @@ env = dbutils.widgets.get("environment")
 
 print(f"Running smoke test in '{env}'")
 
-result = 1 + 1
-assert result == 2
+result = 1 + 10
+assert result == 11
 
-second_check = 2 * 3
-assert second_check == 6
+second_check = 2 * 2
+assert second_check == 4
 
 print("Smoke test passed")
+# feature work
