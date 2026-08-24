c5516096a6b8744e7efeea045ea7621137397733
Stuti-Ganit <stuti@ganitinc.com>
2026-08-24 17:51:45 +0530

    PR for code changes

--- PFL/notebooks/smoke_test.py
+++ PFL/notebooks/smoke_test.py
@@ -6,8 +6,8 @@ env = dbutils.widgets.get("environment")
 
 print(f"Running smoke test in '{env}'")
 
-result = 1 + 1
-assert result == 2
+result = 1 + 10
+assert result == 11
 
 second_check = 2 * 3
 assert second_check == 6
