bdba7ace36efcda88d00bda1f1c8d374377138d2
Stuti-Ganit <stuti@ganitinc.com>
2026-08-27 03:27:43 +0530

    test

--- PFL/notebooks/smoke_v1.py
+++ PFL/notebooks/smoke_v1.py
@@ -7,10 +7,9 @@ env = dbutils.widgets.get("environment")
 print(f"Running smoke test in '{env}'")
 
 result = 1 + 23
-assert result == 24
+assert result == 234
 
-second_check = 2 * 3
-assert second_check == 6
+
 
 print("Smoke test passed")
 # feature work
